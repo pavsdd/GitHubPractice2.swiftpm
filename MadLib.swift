78545e1e306a33b996928f93c2d5d05392b9c4fa
Ben Wojtowicz <Bwojtowicz4483@stu.d214.org>
2023-04-06 14:41:28 -0500

    MadLib done

---
+++ MadLib.swift
@@ -0,0 +1,20 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Ben Wojtowicz on 4/6/23.
+//
+import SwiftUI
+import Foundation
+
+class MadLib{
+    var Noun:String
+    var Verb:String
+    var Adjective:String
+    
+    init(){
+        Noun = ""
+        Verb = ""
+        Adjective = ""
+    }
+}
