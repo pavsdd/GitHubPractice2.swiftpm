f4384f1f61825c6b64be61919f957aa3636d7ace
Ben Wojtowicz <Bwojtowicz4483@stu.d214.org>
2023-04-06 14:10:43 -0500

    Noun view

---
+++ NounView.swift
@@ -0,0 +1,16 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Ben Wojtowicz on 4/6/23.
+//
+import SwiftUI
+import Foundation
+
+struct NounView: View {
+    var body: some View {
+        
+        NavigationLink(Label: "Verb View", destination: VerbView)
+        
+    }
+}
