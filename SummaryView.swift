8287c2520e37060df67ddd9272cfd29e53da4b1d
Pavel Sliziuk <psliziuk6381@stu.d214.org>
2023-04-06 14:49:31 -0500

    i did work

---
+++ SummaryView.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryView.swift
+//  GitHubPractice2
+//
+//  Created by Pavel Sliziuk on 4/6/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text("Then \(enteredWord.Noun) he\(enteredWord.Verb) back \(enteredWord.Adjective)")
+        
+    }
+    
+}
