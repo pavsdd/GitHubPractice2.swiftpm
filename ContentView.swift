3e5f7fcf2545175237fde95934f62b1ce92362b8
Pavel Sliziuk <psliziuk6381@stu.d214.org>
2023-04-06 15:08:55 -0500

    WORKING

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Binding var enteredWord:MadLib
     var body: some View {
         NavigationView {
             NavigationLink("Noun") {
-                NounView()
+                NounView(enteredWord: $enteredWord)
             }
         }
     }
